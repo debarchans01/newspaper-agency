135631711113cfee6d2e36207057b79f8f6f79c7
Raamish <raamish.malik@gmail.com>
2017-11-01 23:28:16 +0530

    signup half done

--- table_def.sql
+++ table_def.sql
@@ -21,7 +21,7 @@ CREATE TABLE [dbo].[User]
 (
 	[U_ID] INT IDENTITY(1000,1) NOT NULL PRIMARY KEY, 
     [Name] NVARCHAR(50) NOT NULL, 
-    [Email-id] NVARCHAR(50) NOT NULL UNIQUE, 
+    [Email_id] NVARCHAR(50) NOT NULL UNIQUE, 
     [Username] NVARCHAR(50) NOT NULL, 
     [Password] NVARCHAR(50) NOT NULL, 
     [Address] NVARCHAR(MAX) NOT NULL, 
