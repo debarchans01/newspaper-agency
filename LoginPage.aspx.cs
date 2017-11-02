490d7fe5b136dbc806c8c0ec940acc96b6c7d61a
debarchans01 <debarchans01@gmail.com>
2017-11-02 09:13:44 +0530

    loginpage error resolved

--- LoginPage.aspx.cs
+++ LoginPage.aspx.cs
@@ -30,35 +30,38 @@ public partial class LoginPage : System.Web.UI.Page
         try
         {
             con1.Open();
-            SqlCommand command = new SqlCommand("select Username,Password,User_type from User where Username=@var1 and Password=@var2 and User_type=@var3", con1);
-            command.Parameters.AddWithValue("@var1",text_Username);
-            command.Parameters.AddWithValue("@var2", text_Password);
+            SqlCommand command = new SqlCommand("select Username,Password,User_type from Users where Username=@var1 and Password=@var2 and User_type=@var3", con1);
+            command.Parameters.AddWithValue("@var1",user_name);
+            command.Parameters.AddWithValue("@var2", pswd);
             command.Parameters.AddWithValue("@var3", user_type);
-            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            /*SqlDataAdapter adapter = new SqlDataAdapter(command);
             DataSet ds = new DataSet();
             adapter.Fill(ds, "user_details");
-            con1.Close();
-            if (ds.Tables["user_details"].Rows.Count == 0)
+            con1.Close();*/
+            SqlDataReader reader;
+            reader = command.ExecuteReader();
+            if (reader.HasRows == false)
             {
                 Error_Label.Text = "Invalid Credentials";
             }
             else
             {
+                reader.Read();
                 if (user_type.Equals("Subscriber"))
                 {
-                    Session["username"] = user_name;
+                    Session["username"] = reader["Username"].ToString();
                     Response.Redirect("UserHomePage.aspx?");
                 }
                 else
                 {
-                    Session["username"] = user_name;
+                    Session["username"] = reader["Username"].ToString();
                     Response.Redirect("EmployeeHomePage.aspx");
                 }
             }
         }
         catch(Exception ex)
         {
-            Error_Label.Text = ex.ToString();
+            Error_Label.Text = user_name + "  " + pswd  + "  " + user_type + "  " +  ex.ToString();
         }
         finally
         {
---
+++ UserHomePage.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class UserHomePage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            Label1.Text = "hello " + (string)Session["username"];
+        }
+    }
+}
